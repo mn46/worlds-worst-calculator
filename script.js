9fb36044ffaa04f827d81296b85ea4e9ea318b4f
Marta <m.nowak033@gmail.com>
2022-08-26 14:08:15 +0200

    result is not a number

--- script.js
+++ script.js
@@ -4,27 +4,50 @@ function start() {
   document.querySelector("#firstnumber").addEventListener("input", readFirstNumber);
   document.querySelector("#secondnumber").addEventListener("input", readSecondNumber);
   document.querySelector("#operator").addEventListener("change", readOperator);
+  //   maybe if statement instead?
 }
 
 let firstnumber;
 let secondnumber;
 let operator;
+let result;
 
 function readFirstNumber() {
-  firstnumber = parseInt(document.querySelector("#firstnumber").value);
+  firstnumber = parseFloat(document.querySelector("#firstnumber").value);
   console.log(firstnumber);
 }
 
 function readSecondNumber() {
-  secondnumber = parseInt(document.querySelector("#secondnumber").value);
+  secondnumber = parseFloat(document.querySelector("#secondnumber").value);
   console.log(secondnumber);
 }
 
 function readOperator() {
-  operator = document.querySelector("#operator").value;
+  obtained = toString(document.querySelector("#operator").value);
   console.log(operator);
+  calculate();
 }
 
-// function calculate(firstnumber, secondnumber) {
+function calculate(firstnumber, secondnumber) {
+  //   result = firstnumber + operator + secondnumber;
 
-// }
+  if (operator == "add") {
+    result = firstnumber + secondnumber;
+  } else if (operator == "sub") {
+    result = firstnumber - secondnumber;
+  } else if (operator == "mul") {
+    result = firstnumber * secondnumber;
+  } else {
+    result = firstnumber / secondnumber;
+  }
+
+  console.log(result);
+
+  document.querySelector("#calculate").addEventListener("click", showResult);
+}
+
+function showResult() {
+  let newLi = document.createElement("li");
+  let seeResult = newLi.appendChild(document.createTextNode(result));
+  document.querySelector("#results").appendChild(seeResult);
+}
