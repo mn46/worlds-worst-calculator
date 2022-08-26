4425cffade7f924eac684648c5a517053fed0426
Marta <m.nowak033@gmail.com>
2022-08-26 12:54:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+start();
+
+function start() {
+  // event listener for click
+  readFirstNumber();
+}
+
+// let firstnumber;
+// let secondnumber;
+
+function readFirstNumber() {
+  // function
+  readSecondNumber();
+}
+
+// function readSecondNumber() {
+//     // function
+//     readOperator()
+// }
+
+// function readOperator() {
+//     // function
+//     calculate()
+// }
+
+// function calculate(firstnumber, secondnumber) {
+
+// }
